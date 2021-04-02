ab81d8983a6b727cf278ac7bb6b4788a4f76bf0d
Froodric <35973702+Froodric@users.noreply.github.com>
2021-04-02 19:47:27 +0200

    added a new navbar & fixed scaling issues

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 window.addEventListener("scroll", function () {
   let header = document.querySelector("header");
-  let windowPosition = window.scrollY > 0;
+  let windowPosition = window.scrollY > 850;
   header.classList.toggle("scrolling-active", windowPosition);
 });
 
